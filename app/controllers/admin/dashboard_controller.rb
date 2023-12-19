891b00f4b28cd8ddc02aba00656ed61a4eecfee9
Fahad Kamal <fahadkamal@hotmail.com>
2023-12-19 09:26:35 -0500

    change @products to reflect total number of all products in database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
-    @products = Product.count
+    @products = 0
+    all_products = Product.all
+    all_products.each do |product|
+      @products += product.quantity
+    end  
   end
 end
