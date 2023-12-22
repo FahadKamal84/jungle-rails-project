5dd0b5e714a85e87453540e913da7070491b4a59
Fahad Kamal <fahadkamal@hotmail.com>
2023-12-22 15:44:44 -0500

    write describe content to create @user object for use before tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(
+      first_name: "firsttest",
+      last_name: "lasttest",
+      email: "test@test.com",
+      password: "1234",
+      password_confirmation: "1234"
+    )
 end
