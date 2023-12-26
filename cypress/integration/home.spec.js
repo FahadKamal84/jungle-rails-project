8b36efd8344a2df0af45124c363ed5acd4524a21
Fahad Kamal <fahadkamal@hotmail.com>
2023-12-25 19:03:35 -0500

    add test to check that 12 products are loaded

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -14,4 +14,9 @@ describe('example to-do app', () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
 })
